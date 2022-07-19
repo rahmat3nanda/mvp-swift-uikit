2b3a13b0cae4e1be1bc07c3163a7dadb3c58e38c
Rahmat Trinanda <53601244+rahmat3nanda@users.noreply.github.com>
2022-07-19 16:20:25 +0700

    Init Project

---
+++ mvp-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvp-swift
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 19/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
